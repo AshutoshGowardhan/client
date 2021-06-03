4cb434f0eb4ab9d9bcb28d1ebae53325a4cffa7c
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-03 22:39:05 +0530

    react router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+const PageOne = () => {
+    return <di>PageOne</di>
+}
+const PageTwo = () => {
+    return <di>
+        <button>Click Me</button>
+    </di>
+}
 const App = () => {
     return (
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+            <div>
+                <Route path="/" exact component={PageOne} />
+                <Route path="/pageTwo" exact component={PageTwo} />
+            </div>
+            </BrowserRouter>
+        </div>
     )
 }
 
