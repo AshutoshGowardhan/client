91befc4f39ce24bc4205066a5f1da6bb76b5b4dc
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-05 16:04:06 +0530

    creating stream using json server

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamDelete from './streams/StreamDelete';
-import StreamShow from './streams/StreamShow';
 import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 import Header from './Header';
+
 const App = () => {
-    return (
+  return (
+    <div className="ui container">
+      <BrowserRouter>
         <div>
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route path="/" exact component={StreamList} />
-                    <Route path="/streams/new" exact component={StreamCreate} />
-                    <Route path="/streams/edit" exact component={StreamEdit} />
-                    <Route path="/streams/delete" exact component={StreamDelete} />
-                    <Route path="/streams/show" exact component={StreamShow} />
-                </div>
-            </BrowserRouter>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
-    )
-}
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default App;
-
-
-//997653619340-isdqg1gcg1hld0ildpfi05nr0j5n9o2c.apps.googleusercontent.com
